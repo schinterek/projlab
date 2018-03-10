b63b760cdfea992792e882363a6a377b5df5298d
Lili <Lili@SUPERPC-24HK6P>
2018-03-10 10:18:33 +0100

    Függvények felvétele

--- projlab/src/projlab/BoxDestination.java
+++ projlab/src/projlab/BoxDestination.java
@@ -1,5 +1,18 @@
 package projlab;
 
-public class BoxDestination {
-
+public class BoxDestination extends Cell implements Visitor {
+	// Az a mezõ, ahova a Box-okat kell eljuttatni.
+	
+	public void accept(Movable m) {
+		// Ha a megfelelõ Box-ot tolják rá, akkor
+		// azt eltünteti a pályáról.
+		
+		// TODO
+	}
+	
+	@Override
+	public Movable getContained() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- projlab/src/projlab/Cell.java
+++ projlab/src/projlab/Cell.java
@@ -1,5 +1,35 @@
 package projlab;
 
-public abstract class Cell {
-
+public abstract class Cell implements Visitor {
+	// Egy mezõt reprezentáló absztrakt osztály.
+	
+	public void accept(Movable m){
+		// Üres függvény, ebben az osztályban úgysem
+		// csinál semmit.
+	}
+	
+	public boolean canArrive(Movable toArrive, Direction dir, Player src){
+		// Ha egy Movable objektum (toArrive) akar érkezni a egy bizonyos
+		// irányból (dir), egy bizonyos játékos által (src),
+		// akkor eldönti, hogy megérkezhet-e a mezõre.
+		
+		// Alapértelmezetten true-t ad vissza.
+		return true;
+	}
+	
+	public boolean move(Movable toMove, Direction dir, Player src){
+		// Ha egy Movable egy bizonyos irányba szeretne lépni,
+		// ez a függvény dönti el, hogy léphet-e.
+		
+		// Alapértelmezetten true-t ad vissza.
+		return true;
+	}
+	
+	public void containedLeft(){
+		// Azt kezeli, hogy egy objektum elhagyta az adott cellát.
+	}
+	
+	public abstract Movable getContained();
+	// Absztrakt függvény, visszaadja, hogy milyen
+	// objektum van rajta.
 }
