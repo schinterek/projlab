53a10edb2644cc2a8cba0be2960c6b28a75e039e
Lili <Lili@SUPERPC-24HK6P>
2018-03-10 11:07:37 +0100

    Függvények felvétele

--- projlab/src/projlab/Game.java
+++ projlab/src/projlab/Game.java
@@ -1,7 +1,23 @@
 package projlab;
 
 public class Game {
-
-	// SAndor
+	// A játék menetét irányítja.
 	
+	public void init(){
+		// Inicializál, létrehozza a szükséges objektumokat.
+		
+		// TODO
+	}
+	
+	public void start(){
+		// A játék indításához szükséges lépéseket végzi el.
+		
+		// TODO
+	}
+	
+	public void gameEnded(){
+		// A játék végét kezeli.
+		
+		// TODO
+	}
 }
