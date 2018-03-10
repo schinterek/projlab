cdbdff0975dbeb9704ac1ff90a4bd7ed94073042
Lili <Lili@SUPERPC-24HK6P>
2018-03-10 12:59:58 +0100

    Függvények felvétele

--- projlab/src/projlab/Switch.java
+++ projlab/src/projlab/Switch.java
@@ -1,5 +1,27 @@
 package projlab;
 
-public class Switch {
+import java.util.zip.ZipEntry;
+
+public class Switch extends Floor implements Visitor {
+	// A kapcsolókat reprezentálja.
+	
+	private boolean isOpen;
+	
+	public boolean accept(Movable m) {
+		// Meghívja a Movable setSwitch metódusát.
+		m.setSwitch();
+		// TODO
+		return true;
+	}
+	
+	public void containedLeft(){
+		// Az isOpen attribútumot false-ra állítja.
+		// ???
+		isOpen = false;
+	}
+	
+	public boolean isOpen(){
+		return isOpen;
+	}
 
 }
