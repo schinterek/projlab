49d8a8ca44a8f3f92d860cf10eb92170c30041be
Lili <Lili@SUPERPC-24HK6P>
2018-03-10 11:13:30 +0100

    Függvények felvétele

--- projlab/src/projlab/Hole.java
+++ projlab/src/projlab/Hole.java
@@ -1,5 +1,12 @@
 package projlab;
 
-public class Hole {
+public class Hole implements Visitor {
+	// A luykakat reprezentálja.
 
+	@Override
+	public boolean accept(Movable m) {		
+		// Meghívja a ráérkezõ Movable-nek a Die() függvényét.
+		// TODO
+		return true;
+	}
 }
--- projlab/src/projlab/Map.java
+++ projlab/src/projlab/Map.java
@@ -1,6 +1,26 @@
 package projlab;
 
+import java.util.ArrayList;
+
 // <<singleton>>
 public class Map {
-
-}
+	// A pályát és a rajta lévõ mezõket valósítja meg.
+	
+	// !!!
+	private ArrayList<Cell> cells;
+	// !!!
+	// Milyen strukturában kéne tárolni?
+	
+	public boolean move(Movable toMove, Direction dir, Player src){
+		// Egy bizonyos Movable egy bizonyos irányba akar lépni.
+		// Ez a függvény mondja meg, hogy arra a cellára
+		// rá lehet-e lépni, és hogy állnak-e már rajta.
+		
+		// !!!
+		// "és hogy állnak-e már rajta?"
+		
+		// TODO
+		return true;
+	}
+	
+}	
