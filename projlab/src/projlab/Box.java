1d7c0a541ee1a0eccbf75ac90429f092bd04018e
Pálinkás Sára <sara.palinkas@gmail.com>
2018-03-18 17:21:34 +0100

    s

--- projlab/src/projlab/Box.java
+++ projlab/src/projlab/Box.java
@@ -3,10 +3,10 @@ package projlab;
 public class Box extends Movable{
 	// Egy dobozt reprezent�l, amit a hely�re kell tolni.
 
+	protected BoxDestination boxdestination;
 	public boolean accept(Movable m){
 		// Ha egy Movable el akarja tolni a Boxot, akkor eld�nti,
 		// hogy el tud-e tol�dni, ezzel t�r vissza. */
-		
 		// TODO
 		return false;
 	}
@@ -23,7 +23,8 @@ public class Box extends Movable{
 	public void setSwitch(){
 		// Ha a Box kapcsol�ra tol�dik, ez a f�ggv�ny kapcsolja
 		// be a kapcsol�t.
-		
+
+		return true;
 		// TODO
 	}
 	
@@ -38,7 +39,9 @@ public class Box extends Movable{
 	public void destinationReached(){
 		// Ha a l�da el�ri a sz�m�ra kijel�lt poz�ci�t a padl�n,
 		// elt�nik.
-		
+		addPoint();
+		BoxCounter.boxDisappeared();
+		Die();
 		// TODO
 	}
 
